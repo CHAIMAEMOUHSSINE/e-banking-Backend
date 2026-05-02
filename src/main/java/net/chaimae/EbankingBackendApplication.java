a5a8fb2023e999cf75ef6b7ca534a512c5dd4ce6
Chaimae Mouhssine <chaimaemouhssine60@gmail.com>
2026-05-02 14:40:50 +0100

    single table

--- src/main/java/net/chaimae/EbankingBackendApplication.java
+++ src/main/java/net/chaimae/EbankingBackendApplication.java
@@ -1,6 +1,11 @@
 package net.chaimae;
 
+import net.chaimae.entities.AccountOperation;
+import net.chaimae.entities.CurrentAccount;
 import net.chaimae.entities.Customer;
+import net.chaimae.entities.SavingAccount;
+import net.chaimae.enums.AccountStatus;
+import net.chaimae.enums.OperationType;
 import net.chaimae.repositories.AccountOperationRepository;
 import net.chaimae.repositories.BankAccountRepository;
 import net.chaimae.repositories.CustomerRepository;
@@ -9,6 +14,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Date;
+import java.util.UUID;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -28,7 +35,39 @@ public class EbankingBackendApplication {
                 customer.setEmail(name + "@gmail.com");
                 customerRepository.save(customer);
             });
+            customerRepository.findAll().forEach(cust->{
+                CurrentAccount currentAccount=new CurrentAccount();
+                currentAccount.setId(UUID.randomUUID().toString());
+                currentAccount.setBalance(Math.random()*90000);
+                currentAccount.setCreatedAt(new Date());
+                currentAccount.setStatus(AccountStatus.CREATED);
+                currentAccount.setCustomer(cust);
+                currentAccount.setOverDraft(9000);
+                bankAccountRepository.save(currentAccount);
+
+                SavingAccount savingAccount=new SavingAccount();
+                savingAccount.setId(UUID.randomUUID().toString());
+                savingAccount.setBalance(Math.random()*90000);
+                savingAccount.setCreatedAt(new Date());
+                savingAccount.setStatus(AccountStatus.CREATED);
+                savingAccount.setCustomer(cust);
+                savingAccount.setInterestRate(5.5);
+                bankAccountRepository.save(savingAccount);
+
+            });
+            bankAccountRepository.findAll().forEach(acc->{
+                for (int i = 0; i <10 ; i++) {
+                    AccountOperation accountOperation=new AccountOperation();
+                    accountOperation.setOperationDate(new Date());
+                    accountOperation.setAmount(Math.random()*12000);
+                    accountOperation.setType(Math.random()>0.5? OperationType.DEBIT: OperationType.CREDIT);
+                    accountOperation.setBankAccount(acc);
+                    accountOperationRepository.save(accountOperation);
+                }
+
+            });
         };
+
     }
 
 }
