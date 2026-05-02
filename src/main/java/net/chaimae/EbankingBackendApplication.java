8fa245317272c45154b584fb4fad950957520ede
Chaimae Mouhssine <chaimaemouhssine60@gmail.com>
2026-05-02 12:26:35 +0100

    BDD test

--- src/main/java/net/chaimae/EbankingBackendApplication.java
+++ src/main/java/net/chaimae/EbankingBackendApplication.java
@@ -1,13 +1,26 @@
 package net.chaimae;
 
+import net.chaimae.repositories.AccountOperationRepository;
+import net.chaimae.repositories.BankAccountRepository;
+import net.chaimae.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EbankingBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EbankingBackendApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository) {
+        BankAccountRepository bankAccountRepository,
+        AccountOperationRepository accountOperationRepository) {
+        return args -> {
+            System.out.println("Hello world");
+        };
     }
 
 }
