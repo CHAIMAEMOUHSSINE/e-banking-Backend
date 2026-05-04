2718ff2800005b19918b6ef6df17b1e5198f73cb
Chaimae Mouhssine <chaimaemouhssine60@gmail.com>
2026-05-04 13:20:43 +0100

    DTOs

---
+++ src/main/java/net/chaimae/dtos/CurrentBankAccountDTO.java
@@ -0,0 +1,17 @@
+package net.chaimae.dtos;
+
+import lombok.Data;
+import net.chaimae.enums.AccountStatus;
+
+
+import java.util.Date;
+
+@Data
+public class CurrentBankAccountDTO extends BankAccountDTO {
+    private String id;
+    private double balance;
+    private Date createdAt;
+    private AccountStatus status;
+    private CustomerDTO customerDTO;
+    private double overDraft;
+}
---
+++ src/main/java/net/chaimae/dtos/SavingBankAccountDTO.java
@@ -0,0 +1,14 @@
+package net.chaimae.dtos;
+import lombok.Data;
+import net.chaimae.enums.AccountStatus;
+
+import java.util.Date;
+@Data
+public class SavingBankAccountDTO extends BankAccountDTO {
+    private String id;
+    private double balance;
+    private Date createdAt;
+    private AccountStatus status;
+    private CustomerDTO customerDTO;
+    private double interestRate;
+}
